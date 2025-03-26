a5b3bb7717ce24299c1ccfb2f6f4660c35365e7d
Oleja123 <jediforce82@gmail.com>
2025-03-26 19:37:41 +0400

    3 глава

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -8,10 +9,20 @@ def index():
     user = {'username': 'Oleja'}
     courses = [
         {'name': 'Пожарная безопасность 2025',
-         'type': 'Безопасность'}, 
+         'type': 'Безопасность'},
         {'name': 'Дискретная математика 2025',
-         'type': 'Учебные'}, 
+         'type': 'Учебные'},
         {'name': 'Функциональное прогаммирование 2025',
-         'type': 'Учебные'}, 
+         'type': 'Учебные'},
     ]
     return render_template('index.html', user=user, courses=courses)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Вход для пользователя {}, запомнить меня = {}'.format(
+        form.username.data, form.remember_me.data))
+        return redirect(url_for('index '))
+    return render_template('login.html', form=form)
