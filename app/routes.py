6088f23bcb47449f62ef68985d3c7f385f741b57
Oleja123 <jediforce82@gmail.com>
2025-03-26 17:39:32 +0400

    Глава 2 ready

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
 from app import app
+from flask import render_template
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Oleja'}
+    courses = [
+        {'name': 'Пожарная безопасность 2025',
+         'type': 'Безопасность'}, 
+        {'name': 'Дискретная математика 2025',
+         'type': 'Учебные'}, 
+        {'name': 'Функциональное прогаммирование 2025',
+         'type': 'Учебные'}, 
+    ]
+    return render_template('index.html', user=user, courses=courses)
