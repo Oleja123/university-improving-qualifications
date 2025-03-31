61271c0b6fd8726a1178d2750cd5c51a656d7be9
Oleja123 <jediforce82@gmail.com>
2025-03-31 16:18:09 +0400

    Сделал ролевую модель на уровне бд

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 from flask_login import UserMixin
 
+
+users_roles = sa.Table(
+    'users_roles',
+    db.metadata,
+    sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('role_id', sa.Integer, sa.ForeignKey('role.id'),
+              primary_key=True)
+)
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -17,34 +27,36 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(
         sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    roles: so.WriteOnlyMapped['Role'] = so.relationship(secondary=users_roles)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def has_role(self, role):
+        query = self.roles.select().where(Role.name == role)
+        return db.session.scalar(query) is not None
+    
+    def add_roles(self, roles):
+        query = sa.select(Role).where(Role.name.in_(roles))
+        res = db.session.scalars(query).all()
+        if res is None:
+            return
+        if res is not list:
+            res = [res]
+        for role in res:
+            if not self.has_role(role.name):
+                self.roles.add(role)
+
 
     def __repr__(self):
         return f'User {self.username}'
-
-class Faculty(db.Model):
+    
+class Role(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str] = so.mapped_column(
-        sa.String(255), index=True, unique=True)
-    departments: so.WriteOnlyMapped['Department'] = so.relationship(
-        back_populates='faculty')
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index = True, unique = True)
 
     def __repr__(self):
-        return f'Faculty {self.name}'
-
-
-class Department(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str] = so.mapped_column(
-        sa.String(255), index=True, unique=True)
-    faculty_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey(Faculty.id), index=True)
-    faculty: so.Mapped[Faculty] = so.relationship(back_populates='departments')
-
-    def __repr__(self):
-        return f'Department {self.name} of Faculty {self.faculty.name}'
+        return f'Role {self.name}'
