1be283656fa129a6326f0c6ad35bd4b1a63acb9d
Oleja123 <jediforce82@gmail.com>
2025-04-02 09:26:45 +0400

    пофиксил декоратор

--- app/decorators/role_decorator.py
+++ app/decorators/role_decorator.py
@@ -5,8 +5,14 @@ from flask import make_response, render_template
 def role_accepted(role):
     def decorator(func):
         def check_role(*args, **kwargs):
-            if current_user.role is None or current_user.role != role:
+            if current_user.role is None or current_user.role.name != role:
                 return make_response(render_template('403.html'), 403)
-            return func(args, kwargs)
+            if len(args) != 0 and len(kwargs) != 0:
+                return func(args, kwargs)
+            if len(args) != 0:
+                return func(args)
+            if len(kwargs) != 0:
+                return func(kwargs)
+            return func()
         return check_role
     return decorator
