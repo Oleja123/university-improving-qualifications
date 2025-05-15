e6817779c3976afc35a26dfe8977559d62c8b666
Oleja123 <jediforce82@gmail.com>
2025-05-15 21:36:40 +0400

    а

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import db
+
 from app.errors import bp
 
 
@@ -7,9 +7,11 @@ from app.errors import bp
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
+
 @bp.app_errorhandler(403)
 def forbidden_error(error):
     return render_template('errors/403.html'), 403
+
 
 @bp.app_errorhandler(500)
 def internal_error(error):
